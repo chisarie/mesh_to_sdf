fd85b8745813861d19ee9f016163c500ff1c9986
Nick Heppert <nick.heppert.pi@tri.global>
2022-10-04 08:17:52 -0700

    Formating

--- mesh_to_sdf/utils.py
+++ mesh_to_sdf/utils.py
@@ -2,63 +2,67 @@ import functools
 import trimesh
 import numpy as np
 
+
 def scale_to_unit_sphere(mesh, get_transform=False):
-    if isinstance(mesh, trimesh.Scene):
+  if isinstance(mesh, trimesh.Scene):
     mesh = mesh.dump(concatenate=True)  #.sum()
 
-    translation = mesh.bounding_box.centroid
+  translation = mesh.bounding_box.centroid
 
-    vertices = mesh.vertices - translation
-    distances = np.linalg.norm(vertices, axis=1)
-    scale = np.max(distances)
-    vertices /= scale
+  vertices = mesh.vertices - translation
+  distances = np.linalg.norm(vertices, axis=1)
+  scale = np.max(distances)
+  vertices /= scale
 
-    return_tuple = (trimesh.Trimesh(vertices=vertices, faces=mesh.faces),)
-    if get_transform:
-        return_tuple += ({"scale": np.max(distances), "translation": translation},)
-    return return_tuple
+  return_tuple = (trimesh.Trimesh(vertices=vertices, faces=mesh.faces),)
+  if get_transform:
+    return_tuple += ({"scale": np.max(distances), "translation": translation},)
+  return return_tuple
+
 
 def scale_to_unit_cube(mesh):
-    if isinstance(mesh, trimesh.Scene):
+  if isinstance(mesh, trimesh.Scene):
     mesh = mesh.dump(concatenate=True)  #.sum()
 
-    vertices = mesh.vertices - mesh.bounding_box.centroid
-    vertices *= 2 / np.max(mesh.bounding_box.extents)
+  vertices = mesh.vertices - mesh.bounding_box.centroid
+  vertices *= 2 / np.max(mesh.bounding_box.extents)
 
-    return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
+  return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
+
 
 # Use get_raster_points.cache_clear() to clear the cache
 @functools.lru_cache(maxsize=4)
 def get_raster_points(voxel_resolution):
-    points = np.meshgrid(
-        np.linspace(-1, 1, voxel_resolution),
-        np.linspace(-1, 1, voxel_resolution),
-        np.linspace(-1, 1, voxel_resolution)
-    )
-    points = np.stack(points)
-    points = np.swapaxes(points, 1, 2)
-    points = points.reshape(3, -1).transpose().astype(np.float32)
-    return points
+  points = np.meshgrid(
+      np.linspace(-1, 1, voxel_resolution), np.linspace(-1, 1, voxel_resolution),
+      np.linspace(-1, 1, voxel_resolution)
+  )
+  points = np.stack(points)
+  points = np.swapaxes(points, 1, 2)
+  points = points.reshape(3, -1).transpose().astype(np.float32)
+  return points
+
 
 def check_voxels(voxels):
-    block = voxels[:-1, :-1, :-1]
-    d1 = (block - voxels[1:, :-1, :-1]).reshape(-1)
-    d2 = (block - voxels[:-1, 1:, :-1]).reshape(-1)
-    d3 = (block - voxels[:-1, :-1, 1:]).reshape(-1)
+  block = voxels[:-1, :-1, :-1]
+  d1 = (block - voxels[1:, :-1, :-1]).reshape(-1)
+  d2 = (block - voxels[:-1, 1:, :-1]).reshape(-1)
+  d3 = (block - voxels[:-1, :-1, 1:]).reshape(-1)
 
-    max_distance = max(np.max(d1), np.max(d2), np.max(d3))
-    return max_distance < 2.0 / voxels.shape[0] * 3**0.5 * 1.1
+  max_distance = max(np.max(d1), np.max(d2), np.max(d3))
+  return max_distance < 2.0 / voxels.shape[0] * 3**0.5 * 1.1
+
 
 def sample_uniform_points_in_unit_sphere(amount):
-    unit_sphere_points = np.random.uniform(-1, 1, size=(amount * 2 + 20, 3))
-    unit_sphere_points = unit_sphere_points[np.linalg.norm(unit_sphere_points, axis=1) < 1]
+  unit_sphere_points = np.random.uniform(-1, 1, size=(amount * 2 + 20, 3))
+  unit_sphere_points = unit_sphere_points[np.linalg.norm(unit_sphere_points, axis=1) < 1]
 
-    points_available = unit_sphere_points.shape[0]
-    if points_available < amount:
-        # This is a fallback for the rare case that too few points are inside the unit sphere
-        result = np.zeros((amount, 3))
-        result[:points_available, :] = unit_sphere_points
-        result[points_available:, :] = sample_uniform_points_in_unit_sphere(amount - points_available)
-        return result
-    else:
-        return unit_sphere_points[:amount, :]
+  points_available = unit_sphere_points.shape[0]
+  if points_available < amount:
+    # This is a fallback for the rare case that too few points are inside the unit sphere
+    result = np.zeros((amount, 3))
+    result[:points_available, :] = unit_sphere_points
+    result[points_available:, :] = sample_uniform_points_in_unit_sphere(amount - points_available)
+    return result
+  else:
+    return unit_sphere_points[:amount, :]
